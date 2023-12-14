cf3f8f4a0b04b7ad89ea86c356f9ccf700a6bfdd
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2023-12-14 16:50:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ function isSameType(value1, value2) {
 
   //your js code here
 }
+console.log(isSameType(1, 3)); // true
+console.log(isSameType("hey", "hello")); // true
+console.log(isSameType(NaN, NaN)); // true
+console.log(isSameType("3", 3)); // false
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
